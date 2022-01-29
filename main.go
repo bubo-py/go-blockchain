6a61977c7e7e92abf8e88095c83643c3e054bfa0
bubo-py <kamil.chruscinski1@gmail.com>
2022-01-29 16:23:04 +0100

    add blockchain struct & add block function

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ type Block struct {
 	PrevHash []byte
 }
 
+type Blockchain struct {
+	blocks []*Block
+}
+
 func (b *Block) DeriveHash() {
 	info := bytes.Join([][]byte{b.Data, b.Hash}, []byte{})
 	hash := sha256.Sum256(info)
@@ -21,6 +25,12 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	block := &Block{[]byte{}, []byte(data), prevHash}
 	block.DeriveHash()
 	return block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := CreateBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
 }
 
 func main() {
